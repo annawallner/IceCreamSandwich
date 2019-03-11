fc726e59eb8af0ab7d695229c880b1c77fda74a3
annwallner <annwallner@edu.aau.at>
2019-03-11 18:09:18 +0100

    Instanzvariablen erstellt

--- IceCreamSandwich/app/src/main/java/com/example/icecreamsandwich/MainActivity.java
+++ IceCreamSandwich/app/src/main/java/com/example/icecreamsandwich/MainActivity.java
@@ -2,8 +2,15 @@ package com.example.icecreamsandwich;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Instanzvariablen eingefügt
+    public TextView textView;
+    public TextView textView2;
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
